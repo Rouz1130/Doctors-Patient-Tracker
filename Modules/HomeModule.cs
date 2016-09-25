bcc0f857c5e06605d4a38cfefa2c880fe706bd17
rouz majlessi <rmajlessi@gmail.com>
2016-09-25 13:51:20 -0700

    add all cshtml files and wrtie first route in nancy

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,4 @@
-using  System.Collections.Generic;
+using System.Collections.Generic;
 using Nancy;
 using Nancy.ViewEngines.Razor;
 
@@ -8,40 +8,14 @@ namespace Appointment
   {
     public Homemodule()
     {
+
       Get["/"] = _ => {
         List<Doctor> AllDoctors = Doctor.GetAll();
         return View["index.cshtml", AllDoctors];
       };
 
-      Get["/patients"] = _ => {
-        List<Patient> AllPatients = Patient.GetAll();
-        return View["patients.cshtml", AllPatients];
-      };
 
-      Get["/doctors"] = _ => {
-        List<Doctor> AllDoctors = Doctor.GetAll();
-        return View["doctors.cshtml", AllDoctors];
-      };
 
-      Get["/doctors/new"] = _ => {
-     return View["doctors_form.cshtml"];
-   };
-
-   Post["/doctors/new"] = _ => {
-        Doctor newDoctor = new Doctor(Request.Form["doctor-name"]);
-        newDoctor.Save();
-        return View["confirm.cshtml"];
-      };
-
-      Get["/patients/new"] = _ => {
-       List<Doctor> AllDoctors = Doctor.GetAll();
-       return View["patients_form.cshtml", AllDoctors];
-     };
-     Post["/patients/new"] = _ => {
-      Patient newPatient = newPatient(Request.Form["patient-name"], Request.Form["doctor-id"]);
-       newPatient.Save();
-       return View["confirm.cshtml"];
-     };
 
 
 
@@ -51,6 +25,3 @@ namespace Appointment
 
 
     }
-
-  }
-}
