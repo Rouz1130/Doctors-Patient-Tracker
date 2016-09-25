dedb867a2acb292412061c6e7b505c9a72af1cd7
rouz majlessi <rmajlessi@gmail.com>
2016-09-24 17:50:18 -0700

    add more routing and create index file and patient and doctors and populate them

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,22 +1,56 @@
-// using  System.Collections.Generic;
-// using Nancy;
-// using Nancy.ViewEngines.Razor;
-//
-// namespace Appointment
-// {
-//   public class Homemodule : NancyModule
-//   {
-//     public Homemodule()
-//     {
-//       Get["/"] = _ => {
-//         List<Doctor> allDoctors = Doctor.GetAll();
-//         return View["index.cshtml"];
-//       };
-//       Get["/patients"] = _ => {
-//         List<Patient> AllPatients = Patient.GetAll();
-//         return View["patients.cshtml", AllPatients];
-//       };
-//     }
-//
-//   }
-// }
+using  System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace Appointment
+{
+  public class Homemodule : NancyModule
+  {
+    public Homemodule()
+    {
+      Get["/"] = _ => {
+        List<Doctor> AllDoctors = Doctor.GetAll();
+        return View["index.cshtml", AllDoctors];
+      };
+
+      Get["/patients"] = _ => {
+        List<Patient> AllPatients = Patient.GetAll();
+        return View["patients.cshtml", AllPatients];
+      };
+
+      Get["/doctors"] = _ => {
+        List<Doctor> AllDoctors = Doctor.GetAll();
+        return View["doctors.cshtml", AllDoctors];
+      };
+
+      Get["/doctors/new"] = _ => {
+     return View["doctors_form.cshtml"];
+   };
+
+   Post["/doctors/new"] = _ => {
+        Doctor newDoctor = new Doctor(Request.Form["doctor-name"]);
+        newDoctor.Save();
+        return View["confirm.cshtml"];
+      };
+
+      Get["/patients/new"] = _ => {
+       List<Doctor> AllDoctors = Doctor.GetAll();
+       return View["patients_form.cshtml", AllDoctors];
+     };
+     Post["/patients/new"] = _ => {
+      Patient newPatient = newPatient(Request.Form["patient-name"], Request.Form["doctor-id"]);
+       newPatient.Save();
+       return View["confirm.cshtml"];
+     };
+
+
+
+
+
+
+
+
+    }
+
+  }
+}
